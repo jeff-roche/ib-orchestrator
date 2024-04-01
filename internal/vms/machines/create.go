3851ef416c1976a17dbb43b0a31cd30eb34a3f43
Jeff Roche <jeroche@redhat.com>
2024-04-01 13:43:37 -0400

    fix: added virt-install command

--- internal/vms/machines/create.go
+++ internal/vms/machines/create.go
@@ -2,6 +2,7 @@ package machines
 
 import (
 	"fmt"
+	"os/exec"
 
 	"snoman/internal/vms/network"
 	vmutils "snoman/internal/vms/utils"
@@ -20,11 +21,36 @@ func CreateVirtualMachine(spec *VirtualMachineSpec) error {
 		}
 	}
 
-	lvc, err := vmutils.GetLibvirtConnection()
-	if err != nil {
-		return fmt.Errorf("unable to initialize libvirt connection: %w", err)
+	if err := startVirtualMachine(spec); err != nil {
+		return fmt.Errorf("unable to start virtual machine: %w", err)
 	}
-	defer lvc.Close()
 
-	return fmt.Errorf("CreateVirtualMachine %w", vmutils.ErrNotImplemented)
+	return nil
+}
+
+func startVirtualMachine(spec *VirtualMachineSpec) error {
+	args := []string{
+		"--connect", "qemu:///system",
+		"-n", spec.Name,
+		"-r", fmt.Sprint(spec.RAM),
+		"--vcpus", fmt.Sprint(spec.CPU),
+		"--os-variant", "rhel8.1",
+		"--import",
+		fmt.Sprintf("--network=network:\"%s\",mac=\"%s\"", spec.Network.Name, spec.Network.MacAddress),
+		"--graphics=none",
+		"--events", "on_reboot=restart",
+		"--cdrom", spec.BipSpec.IsoPath,
+		"--disk", fmt.Sprintf("pool=\"%s\",size=\"%d\"", spec.Disk.Pool, spec.Disk.Size),
+		"--check", "disk_size=off",
+		"--boot", "hd,cdrom",
+		"--noautoconsole",
+		"--wait=-1",
+	}
+
+	cmd := exec.Command("virt-install", args...)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error executing virt-install: %w", err)
+	}
+
+	return nil
 }
